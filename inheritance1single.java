26c422e58008c5a4fa12a839e54ce649da9e93de
Vishal Hedau <vishalhedau3069@gmail.com>
2023-01-25 12:58:51 +0530

    single inheritance in java

---
+++ inheritance1single.java
@@ -0,0 +1,30 @@
+
+class inherit1
+{
+int a;
+}
+
+class inherit2 extends inherit1
+{
+	void set()
+	{
+	  a=12;
+	}
+
+	void dis()
+	{
+	System.out.println(a);
+	}
+}
+
+public class inheritance1single
+{
+public static void main(String[] args)
+{
+	inherit2 ob = new inherit2();
+	//ob.a=12;
+	ob.set();
+	ob.dis();
+
+}
+}
