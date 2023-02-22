2f1e39b33135d065633c696e3e9e94e2638a2b36
Vishal Hedau <vishalhedau3069@gmail.com>
2023-02-22 12:00:12 +0530

    Show databases in command line

---
+++ vishal java assignment 16-17 feb/show_database.sql
@@ -0,0 +1,19 @@
+// show databases
+
+
+
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| myfamilydb         |
+| mysql              |
+| performance_schema |
+| prac01             |
+| somaliadb          |
+| somaliadb2         |
+| test               |
+| vishaldb           |
++--------------------+
+9 rows in set (0.01 sec)
