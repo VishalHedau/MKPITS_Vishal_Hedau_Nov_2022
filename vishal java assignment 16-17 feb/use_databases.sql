862d22e0bb80f37dc4a321c24aece4b2cdc2aaaa
Vishal Hedau <vishalhedau3069@gmail.com>
2023-02-22 12:03:19 +0530

    use databases

---
+++ vishal java assignment 16-17 feb/use_databases.sql
@@ -0,0 +1,14 @@
+//use databases
+
+
+mysql> use myfamilydb;
+Database changed
+mysql> show tables;
++----------------------+
+| Tables_in_myfamilydb |
++----------------------+
+| address              |
+| familymembers        |
+| myfriends            |
++----------------------+
+3 rows in set (0.01 sec)
