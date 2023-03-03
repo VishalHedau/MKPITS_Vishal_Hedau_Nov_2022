043a5ef3ebfa2ea174d7d9667f0b2b17564ca258
Vishal Hedau <vishalhedau3069@gmail.com>
2023-03-03 11:48:04 +0530

    read the value and display

---
+++ vishal java assignment 16-17 feb/read_the_value_and_display.java
@@ -0,0 +1,30 @@
+/*Write a program to read the value of an integer m and display the value
+of m is 1 when m is larger than 0, 0 when m is 0 and -1 when m is less
+than 0. */
+
+import java.util.Scanner;
+
+class read_the_value_and_display
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value");
+		int m=sc.nextInt();
+		if(m>0)
+		{
+			m=1;
+			System.out.println("m is "+m);
+		}
+		else if(m==0)
+		{
+			m=0;
+			System.out.println("m is "+m);
+		}
+		else
+		{
+			m=-1;
+			System.out.println("m is "+m);
+		}
+	}
+}
