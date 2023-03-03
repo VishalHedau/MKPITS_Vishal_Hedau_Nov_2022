f0afe3b53840de16dbf7acf8b2cc525873a25ac3
Vishal Hedau <vishalhedau3069@gmail.com>
2023-03-03 11:20:16 +0530

    java program

---
+++ calc_avg_of_input.java
@@ -0,0 +1,19 @@
+/*Write a Java  program that takes four numbers as input to calculate and
+print the average.  */
+
+
+import java.util.Scanner;
+class calc_avg_of_input
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the 4 numbers you want to find the average");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int d=sc.nextInt();
+		float avg=(float)(a+b+c+d)/4;
+		System.out.println("Average is "+avg);
+	}
+}
