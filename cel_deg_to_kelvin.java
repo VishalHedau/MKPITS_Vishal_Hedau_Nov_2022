112c4df8e261fdaf7ea7f47ed4aa534124d6b554
Vishal Hedau <vishalhedau3069@gmail.com>
2023-03-03 11:36:14 +0530

    java programs

---
+++ cel_deg_to_kelvin.java
@@ -0,0 +1,18 @@
+/*Write a Java program to convert from celsius degrees to Kelvin and
+Fahrenheit. */
+
+import java.util.Scanner;
+
+class cel_deg_to_kelvin
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the  temperature in Celsius");
+		float tem=sc.nextFloat();
+		float F=((tem*9)/5)+32;
+		double K=tem+273.15;
+		System.out.println("celsius degrees to Kelvin : "+K);
+		System.out.println("celsius degrees to Fahrenheit : "+F);
+	}
+}
