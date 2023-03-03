dcebdc131fc3a04a97d082f39abb489724e9876a
Vishal Hedau <vishalhedau3069@gmail.com>
2023-03-03 20:58:57 +0530

    division of two numbers in java

---
+++ vishal java assignment 16-17 feb/result_of_dividing_of_numbers.java
@@ -0,0 +1,37 @@
+// Write a Java  program to print the result of dividing two numbers.
+
+import java.util.*;
+
+public class result_of_dividing_of_numbers{
+public static void main(String [] args){
+
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter 1st number : ");
+	double a = sc.nextDouble();
+	System.out.print("Enter 2nd number : ");
+	double b = sc.nextDouble();
+
+	double divide;
+
+	divide = a/b;
+
+	System.out.println("The division of the numbers is : "+divide);
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
