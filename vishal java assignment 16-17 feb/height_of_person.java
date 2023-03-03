b0de96886852263f668cb82aa02f73beeb4e96db
Vishal Hedau <vishalhedau3069@gmail.com>
2023-03-03 11:50:56 +0530

    height of a person

---
+++ vishal java assignment 16-17 feb/height_of_person.java
@@ -0,0 +1,30 @@
+/* Write a program to accept the height of a person in
+centimeter and categorize the person according to their height.*/
+
+import java.util.Scanner;
+
+class height_of_person
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Person Height in cm");
+		float h=sc.nextFloat();
+		if(h<150)
+		{
+			System.out.println("Person is shorter in height");
+		}
+		else if(h>=150 && h<165)
+		{
+			System.out.println("Person is average in height");
+		}
+		else if(h>=165 && h<195)
+		{
+			System.out.println("Person is taller in height");
+		}
+		else
+		{
+			System.out.println("Person is way taller than tall person in height");
+		}
+	}
+}
