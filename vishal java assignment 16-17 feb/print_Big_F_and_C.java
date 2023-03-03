9a028eba50b4afa8c921ea6904a96a145ff1a9c9
Vishal Hedau <vishalhedau3069@gmail.com>
2023-03-03 21:49:56 +0530

    print Big F and C

---
+++ vishal java assignment 16-17 feb/print_Big_F_and_C.java
@@ -0,0 +1,39 @@
+/* Write a C program to print a block F using hash (#), where the F has a height of six characters and width of five and four characters. And also to print a big 'C'.
+    #######
+#
+#
+#####
+#
+#
+#
+
+   ######
+  ## ####
+ #
+ #
+ #
+ #
+ #
+  ##  ##    */
+
+  public class print_Big_F_and_C {
+  public static void main (String [] args){
+	  System.out.println("#####");
+	  System.out.println("#");
+	  System.out.println("#####");
+	  System.out.println("#");
+	  System.out.println("#");
+	  System.out.println("#");
+	  System.out.println();
+	  System.out.println();
+	  System.out.println("#####");
+	  System.out.println("#");
+	  System.out.println("#");
+	  System.out.println("#");
+	  System.out.println("#");
+	  System.out.println("#####");
+
+
+  }
+  }
+
