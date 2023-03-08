7aee76de2f60cdd5dad471a2cb1af32eab250e6e
Vishal Hedau <vishalhedau3069@gmail.com>
2023-03-08 22:09:10 +0530

    factorials in java

---
+++ vishal java assignment 16-17 feb/java_factorial.java
@@ -0,0 +1,27 @@
+//factorial program by using java.....
+
+public class java_factorial {
+
+    public static int factorial() {
+
+        int f = 1;
+        int x;
+       // return num;
+
+        for (int i = 0; i <= x; i++) {
+
+            f = f * i;
+        }
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+	java_factorial ob = new java_factorial();
+    int num = factorial(9);
+        System.out.println(num);
+
+    }
+}
