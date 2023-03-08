33051c8a9e846f400460be71941e529581812d26
Vishal Hedau <vishalhedau3069@gmail.com>
2023-03-08 21:21:12 +0530

    sum of the two numbers in java by using method

---
+++ vishal java assignment 16-17 feb/sum_of_the_two_no.java
@@ -0,0 +1,14 @@
+//basics of java sum of two numbers.
+
+public class sum_of_the_two_no {
+
+public static void my_method(){
+    int a =4 , b =5;
+    int sum = a+b;
+    System.out.println(sum);
+
+}
+    public static void main(String[] args) {
+        my_method();
+    }
+}
