2c1fc3f3d3ee91c07953884e8bcdf0eac8c0b671
Vishal Hedau <vishalhedau3069@gmail.com>
2023-03-08 21:40:12 +0530

    parameters passing String and integer through method

---
+++ vishal java assignment 16-17 feb/String_int_passing.java
@@ -0,0 +1,18 @@
+// java program by string and int passing method..
+
+
+public class String_int_passing {
+
+    public static void my_method(String fname , int x){
+
+        System.out.println(fname +" is "+ x);
+
+    }
+    public static void main(String[] args) {
+        my_method("vishal",23);
+
+        my_method("paritosh",23);
+
+        my_method("kunal",22);
+    }
+}
