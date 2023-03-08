e25ad31bfafd432c37f0eab030d5327b9133f17e
Vishal Hedau <vishalhedau3069@gmail.com>
2023-03-08 21:31:05 +0530

    values passing by arguments in java

---
+++ vishal java assignment 16-17 feb/Argument_passing_values.java
@@ -0,0 +1,15 @@
+// java program by using arguments
+
+
+public class Argument_passing_values {
+
+public static int my_method(int x, int y){
+
+    return x+y;
+
+}
+    public static void main(String[] args) {
+     int sum =  my_method(5,3);
+        System.out.println(sum);
+    }
+}
