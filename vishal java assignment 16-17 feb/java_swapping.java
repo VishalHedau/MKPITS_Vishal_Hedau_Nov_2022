7c8ce5e4a1b22ec0d833bb2fc8033f3724a98275
Vishal Hedau <vishalhedau3069@gmail.com>
2023-03-08 22:13:32 +0530

    swapping the numbers by using functions

---
+++ vishal java assignment 16-17 feb/java_swapping.java
@@ -0,0 +1,24 @@
+//// swap number by using functions
+
+
+
+public class java_swapping {
+public static void swap(int x , int y){
+    int c;
+    c = x;//c =5
+    x = y;//a =4
+    y = c;//b =5;
+    System.out.println("value after swap is a= "+x+"and b="+y);
+    System.out.println(x);
+    System.out.println(y);
+
+
+
+}
+
+    public static void main(String[] args) {
+        int a =5;
+        int b =4;
+        swap(a,b);
+    }
+}
