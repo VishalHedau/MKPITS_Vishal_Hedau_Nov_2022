97381adfd5e596c5653219585e6c41625d1ee1cf
Vishal Hedau <vishalhedau3069@gmail.com>
2023-03-08 11:23:22 +0530

    creating object and classes

---
+++ vishal java assignment 16-17 feb/Student_object_creating.java
@@ -0,0 +1,15 @@
+//Program to demonstrate the creation of a class and object in Java.
+
+class Student_object_creating{
+
+ int id;
+ String name;
+
+ public static void main(String args[]){
+
+  Student_object_creating s1=new Student_object_creating();
+
+  System.out.println(s1.id);
+  System.out.println(s1.name);
+ }
+}
