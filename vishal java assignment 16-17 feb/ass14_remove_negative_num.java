1edaf557d5ad3d342aea0cacac46166b81616cf1
Vishal Hedau <vishalhedau3069@gmail.com>
2023-03-10 16:45:48 +0530

    java program to remove the negative number

---
+++ vishal java assignment 16-17 feb/ass14_remove_negative_num.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class ass14_remove_negative_num{
+
+
+public static void main(String [] argus){
+
+int num;
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter The negative Num: ");
+float numm = sc.nextFloat();
+
+if(numm<0){
+
+numm = -numm;
+	//System.out.println("Num is :"+a);
+	System.out.println("Num is :"+numm);
+
+	}
+
+else{
+
+	System.out.println("NO neeed of change");
+
+	}
+}
+}
