b45973b2f57275c10077d4eacd7e6660ef3e38ac
Vishal Hedau <vishalhedau3069@gmail.com>
2023-03-10 16:06:09 +0530

    insert the elements in an array

---
+++ vishal java assignment 16-17 feb/array_insert_elements.java
@@ -0,0 +1,36 @@
+//Write a program to INSERT the ELEMENTS in an Array
+
+
+import java.util.*;
+
+public class array_insert_elements{
+
+
+public static void main(String [] args){
+
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter the size : ");
+int size = sc.nextInt();
+
+int a[] = new int [size];
+System.out.println("enter the "+size+"elements");
+
+for(int i=0;i<a.length;i++){
+
+	a[i] = sc.nextInt();
+
+}
+	
+for(int i=0;i<a.length;i++){
+
+System.out.println("Elements are "+a[i]);
+
+}
+
+}
+
+
+
+
+}
