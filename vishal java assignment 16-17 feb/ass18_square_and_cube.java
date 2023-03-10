71ecea932ba58499f82baf050e4f302563e66b77
Vishal Hedau <vishalhedau3069@gmail.com>
2023-03-10 17:02:58 +0530

    java program to calculate the square and cube of the number

---
+++ vishal java assignment 16-17 feb/ass18_square_and_cube.java
@@ -0,0 +1,27 @@
+public class ass18_square_and_cube{
+
+public static void main(String [] argus){
+
+
+int i;
+
+
+
+for(i=0;i<=20;i++){
+
+int sq,cu;
+
+sq = i*i;
+
+cu = i*i*i;
+
+	System.out.println(+i+" "+cu+" "+sq+" ");
+
+	}
+
+
+}
+
+
+
+}
