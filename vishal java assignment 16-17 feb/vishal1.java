53df59d2c47bbd6ea731dc4e247a606b24a0ae85
Vishal Hedau <vishalhedau3069@gmail.com>
2023-03-10 17:11:38 +0530

    java program to check the multiple of elements

---
+++ vishal java assignment 16-17 feb/vishal1.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class vishal1{
+
+public static void main(String [] argu){
+
+Scanner sc = new Scanner(System.in);
+
+System.out.print("enter first num :");
+int a = sc.nextInt();
+
+System.out.print("enter Second num :");
+int b = sc.nextInt();
+
+public void multiple()
+{
+
+	if(a%b==0){
+
+		System.out.println("b is multiple of a"+b);
+
+		}
+	else{
+
+		System.out.println("b is not a multiple of a");
+		}
+
+}
+
+public class div{
+
+public static void main (String [] argu){
+
+		java.ob = new java();
+		ob.multiple();
+
+	}
+
+
+}
+}
+
