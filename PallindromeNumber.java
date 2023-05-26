75688355b289753cd9efa13378f8d71fd4f37d1f
VishalHedau <118975915+VishalHedau@users.noreply.github.com>
2023-05-26 13:14:15 +0530

    pallindrome Number

---
+++ PallindromeNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PallindromeNumber {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to check : ");
+        int num = sc.nextInt();
+
+        int org_num = num;
+
+        int rev = 0;
+         while(num!=0){
+             rev = rev*10 + num%10;
+             num = num/10;
+         }
+
+         if (org_num==rev){
+             System.out.println("This is pallindrome");
+         }else{
+             System.out.println("This is Not-Pallindrome");
+         }
+    }
+}
